c4d362f74d9626ef1601c863fbeed598163a40bd
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-02 02:20:44 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Majestic FIB Forms</title>
+        <meta name="description" content="Система подачи заявок FIB" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <style jsx global>{`
         * {
           margin: 0;
