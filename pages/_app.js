82e4ac36fd0cdfa1c1b28976eacef6ca2eeb4c3b
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-06 05:28:30 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>FIB Forms</title>
-        <meta name="description" content="Система подачи заявок FIB" />
+        <meta name="description" content="Система подачи заявок Орг.FIB" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
