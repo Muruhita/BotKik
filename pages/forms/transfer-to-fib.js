11adbda158313fb0dc8f0d27a546fa761601a01e
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-06 12:54:25 +0700

    Update transfer-to-fib.js

--- pages/forms/transfer-to-fib.js
+++ pages/forms/transfer-to-fib.js
@@ -33,7 +33,7 @@ export default function TransferToFibForm() {
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label>Имя Фамилия | Статик ID</label>
-              <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} required placeholder="Например: Sanya Suspect 270726" />
+              <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} required placeholder="Например: Name Surname 123456" />
             </div>
             <div className="form-group">
               <label>Одобрение от начальства (ссылка)</label>
