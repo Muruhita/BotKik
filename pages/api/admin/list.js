51db38e9c85969a1928cb72cd938d781cff86244
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-06 11:08:28 +0700

    Update list.js

--- pages/api/admin/list.js
+++ pages/api/admin/list.js
@@ -1,4 +1,4 @@
-import { getAllBannedUsers, isFormSubmissionActive } from '../../../lib/antispam';
+import { getAllBannedUsers, isFormSubmissionActive, getAllFormStatuses } from '../../../lib/antispam';
 import { verifyToken } from '../../../lib/discord';
 
 const ADMIN_IDS = ['1018113109346504744', '555380718566506506', '260076815970729985'];
@@ -10,5 +10,7 @@ export default async function handler(req, res) {
 
   const bannedUsers = await getAllBannedUsers();
   const formsActive = await isFormSubmissionActive();
-  res.status(200).json({ bannedUsers, formsActive });
+  const formStatuses = await getAllFormStatuses();
+
+  res.status(200).json({ bannedUsers, formsActive, formStatuses });
 }
