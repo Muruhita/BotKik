3ddfadc6696b3187c2fdbf71626f324123c7d91e
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-06 12:56:16 +0700

    Update transfer.js

--- pages/forms/transfer.js
+++ pages/forms/transfer.js
@@ -127,11 +127,11 @@ export default function TransferForm() {
       <div className="form-page">
         <button onClick={() => router.push('/dashboard')} className="back-btn">← Назад к выбору</button>
         <div className="form-container">
-          <h1>🔄 Перевод в отдел</h1>
+          <h1>🔀 Перевод в отдел</h1>
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label>Имя Фамилия + Статик *</label>
-              <input type="text" required value={formData.fullName} onChange={(e) => setFormData({...formData, fullName: e.target.value})} placeholder="Например: Sanya Suspect 270726" />
+              <input type="text" required value={formData.fullName} onChange={(e) => setFormData({...formData, fullName: e.target.value})} placeholder="Например: Name Surname 123456" />
             </div>
             <div className="form-group">
               <label>Ваш ранг *</label>
