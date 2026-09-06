ec9d5675ea6871bb753c6ee9f59cce4ac9380070
Mur <195444383+Muruhita@users.noreply.github.com>
2026-09-06 14:05:27 +0700

    Update stats.js

--- pages/api/admin/stats.js
+++ pages/api/admin/stats.js
@@ -1,7 +1,7 @@
 import redis from '../../../lib/redis';
 import { verifyToken } from '../../../lib/discord';
 
-const ADMIN_IDS = ['1018113109346504744', '555380718566506506', '260076815970729985'];
+const ADMIN_IDS = ['1018113109346504744', '555380718566506506', '260076815970729985', '797111731864207360'];
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
